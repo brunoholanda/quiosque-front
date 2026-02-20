d2e9be6321c4b4cff0c39b1107a4ccabc03e9407
brunoholanda <holanda_rodrigues@hotmail.com>
2026-02-20 15:04:57 -0300

    routes

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_PUBLIC_API_BASE_URL
-    ? `${import.meta.env.VITE_PUBLIC_API_BASE_URL}`
+    ? `${import.meta.env.VITE_PUBLIC_API_BASE_URL}/api`
     : '/api',
 })
 
